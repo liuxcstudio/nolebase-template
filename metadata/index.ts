210118634079718dad18251febc9498da81fa5bd
liuxc <136138957+liuxcstudio@users.noreply.github.com>
2025-06-11 13:07:45 +0800

    Update index.ts

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,6 +1,6 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
+export const siteName = 'XY'sBlog'
+export const siteShortName = 'XY'sBlog'
 export const siteDescription = '记录回忆，知识和畅想的地方'
 
 /** 文档所在目录 */
@@ -8,9 +8,6 @@ export const include = ['笔记', '生活']
 
 /** Repo */
 export const githubRepoLink = 'https://github.com/nolebase/nolebase'
-/** Discord */
-export const discordLink = 'https://discord.gg/XuNFDcDZGj'
-
 /** 无协议前缀域名 */
 export const plainTargetDomain = 'nolebase.ayaka.io'
 /** 完整域名 */
